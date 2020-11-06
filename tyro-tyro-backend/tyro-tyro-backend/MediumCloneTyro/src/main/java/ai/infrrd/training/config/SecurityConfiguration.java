7a76d412a06b32043721d2bd254bad9ba34df0e8
Kavil514 <kav514.jain@gmail.com>
2020-11-06 14:04:18 +0530

    Changed username to Email for login

---
+++ tyro-tyro-backend/tyro-tyro-backend/MediumCloneTyro/src/main/java/ai/infrrd/training/config/SecurityConfiguration.java
@@ -0,0 +1,47 @@
+package ai.infrrd.training.config;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import ai.infrrd.training.service.MongoUserDetailsService;
+
+@Configuration
+@EnableConfigurationProperties
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	
+
+	@Autowired
+	private MongoUserDetailsService userDetailsService;
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+		auth.userDetailsService(userDetailsService);
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		http.csrf().disable();
+		http.authorizeRequests()
+		.antMatchers("/user/signup").permitAll()
+		.antMatchers("/user/login").permitAll();
+	}
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+ 
+//    @Override
+//    public void configure(AuthenticationManagerBuilder builder) throws Exception {
+//      builder.userDetailsService(userDetailsService);
+//    }
+
+}
